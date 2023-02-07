c0d6d9dfdcdcd54cb847b6b1917b93ef433decd4
Dragan Simonovic <dragan.simonovic.bp@gmail.com>
2023-02-07 23:20:19 +0100

    handling form submission

--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,10 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
 }
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredName = formData.get("player-name");
+  console.log(enteredName);
+}
